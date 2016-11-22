912518273ddc85c0f1b6ef4f69d9d10e6148e982
mikefab <mikefabrikant@gmail.com>
2016-11-22 15:28:31 -0500

    Add async lib

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// After sftp2blob fetches new mobility zip files from azure and uploads to azure blob storage
+// this program does aggregations
+
 var ArgumentParser = require('argparse').ArgumentParser;
 var bluebird = require('bluebird');
 var queue = require('./lib/queue');
@@ -5,6 +8,7 @@ var config = require('./config');
 var csv_columns = config.columns;
 var db_fields = config.db_fields;
 var azure = require('./aggregate_airport_to_admin/azure_storage');
+var async = require('async');
 // var aggregate = require('./aggregate_airport_to_admin/aggregate_airport_to_admin');
 
 function aggregate_new_blobs(collection) {
@@ -15,12 +19,14 @@ function aggregate_new_blobs(collection) {
     .catch(function(err) {
       return reject(err);
     })
+    // At the moment, only process zipped files.
     .then(function(blobs) {
       blobs = blobs.filter(function(e) {
         return e.match(/.gz$/);
       });
 
       blobs.forEach(function(blob) {
+        console.log(blob);
         queue.queue.push(
           {
             collection: collection,
@@ -58,27 +64,63 @@ function main() {
     {help: 'Kind of mobility being parsed'}
   );
 
-  // var args = parser.parseArgs();
-  // var file = args.file;
-  // var kind = args.kind;
-
-  azure.get_collection_names()
-  .catch(function(err) { console.log(err);})
-  .then(function(collections) {
-    collections = [collections[4]];
-    // Create a storage container for each collection if it doesn't already exist.
-    azure.create_storage_containers(collections)
-    .then(function() {
-      // Iterate through collections, and aggregate new blobs to collection
-      bluebird.map(collections, function(collection, index) {
-        return aggregate_new_blobs(collection);
-      }, {concurrency: 1})
+  async.waterfall([
+    function(callback) {
+      // Retrieves list of blobs in a collection: 'booking', 'search', 'schedule'..etc.
+      azure.get_collection_names()
       .catch(function(err) { console.log(err);})
-      .then(function() {
-        console.log('Done!');
+      .then(function(collections) {
+        callback(null, collections);
       });
-    });
+    },
+    function(collections, callback) {
+      collections = [collections[4]];
+      // Create a storage container for each collection on azure
+      azure.create_storage_containers(collections)
+      .then(function(results) {
+        console.log(results);
+        // Iterate through collections, and aggregate new blobs to collection
+        bluebird.map(collections, function(collection, index) {
+          return aggregate_new_blobs(collection);
+        }, {concurrency: 1})
+        .catch(function(err) {
+          console.log(err);
+        })
+        .then(function() {
+          callback();
+        });
+      });
+    }
+  ], function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+    process.exit();
   });
 }
+
+//   // var args = parser.parseArgs();
+//   // var file = args.file;
+//   // var kind = args.kind;
+//   // Retrieves list of blobs in a collection:
+//   azure.get_collection_names()
+//   .catch(function(err) { console.log(err);})
+//   .then(function(collections) {
+//     collections = [collections[4]];
+//     // Create a storage container for each collection if it doesn't already exist.
+//     azure.create_storage_containers(collections)
+//     .then(function() {
+//       // Iterate through collections, and aggregate new blobs to collection
+//       bluebird.map(collections, function(collection, index) {
+//         return aggregate_new_blobs(collection);
+//       }, {concurrency: 1})
+//       .catch(function(err) { console.log(err);})
+//       .then(function() {
+//         console.log('Done!');
+//       });
+//     });
+//   });
+// }
 
 main();
