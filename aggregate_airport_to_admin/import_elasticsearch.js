904455bd88f040feea52f6e72c5d5a2eb8214f09
mikefab <mikefabrikant@gmail.com>
2016-11-26 00:06:27 +0000

    Change separator on the fly

--- aggregate_airport_to_admin/import_elasticsearch.js
+++ aggregate_airport_to_admin/import_elasticsearch.js
@@ -6,7 +6,8 @@ var LineByLineReader = require('line-by-line');
 var csv_helper = require('./csv_helper');
 var u = require('underscore');
 var config = require('../config');
-
+var separator = '\t';
+var data;
 function import_to_elastic_search(es, options, file, db_fields, csv_columns, lookup) {
   var indexes;
   var counter = 0;
@@ -17,22 +18,26 @@ function import_to_elastic_search(es, options, file, db_fields, csv_columns, loo
       console.log(err);
          // 'err' contains error object
     });
-
+    console.log('About to read lines!');
     lr.on('line', function(line) {
-      var data = line.split('^');
       if (counter === 0) {
+        if (line.match(/\^/)) {
+          separator = '^';
+	}
+        data = line.split(separator);
         // It's the first line...
         // Get indexes of needed columns
         // For instance:
         // { origin: 0, destination: 5, pax: 11, date: 15 }
         indexes = csv_helper.find_indexes_for_columns(data, csv_columns);
       } else if (counter !== 0 && data.length > 1) {
+        data = line.split(separator);
         // Origin airport
         var origin = data[indexes.origin];
         // Destination airport
         var destination = data[indexes.destination];
         // Date of journey
-        var date = data[indexes.date].split(/\s+/)[0];
+        //var date = data[indexes.date].split(/\s+/)[0];
         // Number of passengers
         var pax = parseInt(data[indexes.pax], 10);
 
@@ -47,12 +52,11 @@ function import_to_elastic_search(es, options, file, db_fields, csv_columns, loo
         // var destination_a1 = admin.get_admin(destination, 1);
         var destination_a2 = admin.get_admin(lookup, destination, 2);
         var row;
-        if (origin_a2 && destination_a2 && pax && date) {
+        if (origin_a2 && destination_a2 && pax) {
           row = u.flatten([
             origin_a2,
             destination_a2,
-            pax,
-            date
+            pax
           ]);
 
           var json = db_fields.reduce(function(h, e, i) {
@@ -90,7 +94,7 @@ function import_to_elastic_search(es, options, file, db_fields, csv_columns, loo
     lr.on('end', function() {
       if (records.length > 0) {
         bulk_es_insert(es, options, records)
-        .catch(function(err) { return reject(err); })
+        .catch(function(err) {console.log('Error', err); return reject(err); })
         .then(function() {
           resolve();
         });
@@ -127,22 +131,29 @@ exports.import_to_elastic_search = function(es_index, kind, file, db_fields, csv
 
 // Check if index for this type exists, and then delete if needed
 function prepare_index(es, options) {
+  console.log('About to prepare index', options);
   return new Promise(function(resolve, reject) {
+    console.log('About to test existence of index', options);
     es.exists(options, function(err, response) {
       if (err) {
+	console.log('Problem checking existence');
         return reject(err);
       }
       if (response.exists) {
+        console.log('Index exists, so delete!');
         es.indices.deleteIndex(
           {_index: 'mobilities'},
           function(err, result) {
             if (err) {
+              console.log(err);
               return reject(err);
             }
+            console.log(result);
             resolve();
           }
         );
       }
+      console.log('No index to delete!');
       resolve();
     });
   });
