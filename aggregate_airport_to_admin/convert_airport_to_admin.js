8beb93c792da13be575b0c48969b828f11d333bf
mikefab <mikefabrikant@gmail.com>
2016-12-05 16:17:16 +0000

    Don't need admin_name

--- aggregate_airport_to_admin/convert_airport_to_admin.js
+++ aggregate_airport_to_admin/convert_airport_to_admin.js
@@ -2,22 +2,25 @@
 
 exports.get_admin = function(lookup, airport, admin_level) {
     // var admin = admin_level === 1 ? get_admin_1(airport) : get_admin_2(airport);
-    var admin = lookup[airport];
+    var admin = null;
+    if (lookup[airport]) {
+      admin = lookup[airport];
+    }
     if (admin) {
       if (lookup[airport]) {
-        var iso = lookup[airport].ISO;
+        var iso = admin.ISO;
         // These are admin codes.
-        var id_0 = lookup[airport].ID_0;
-        var id_1 = lookup[airport].ID_1;
-        var id_2 = lookup[airport].ID_2;
-	var pub_src = lookup[airport].pub_src;
+        var id_0 = admin.ID_0;
+        var id_1 = admin.ID_1;
+        var id_2 = admin.ID_2;
+	var pub_src = admin.pub_src;
         // Create admin id with country ISO, lowercase admin name, and admin codes.
         // Example: usa-travis-244_44-2754
         // Temp hack 
-        var admin_name = admin.NAME_2 || admin.NAME_1 || admin.NAME_0 || admin.NAME_ISO || admin.NOMBRE_MPI;
+        //var admin_name = admin.NAME_2 || admin.NAME_1 || admin.NAME_0 || admin.NAME_ISO || admin.NOMBRE_MPI;
 
-        var admin_name_no_space = admin_name.replace(/(\s+|-|\.)/g, '_').toLowerCase();
-        var admin_name_modified = admin_name_no_space.replace(/'/g, '_').toLowerCase();
+        //var admin_name_no_space = admin_name.replace(/(\s+|-|\.)/g, '_').toLowerCase();
+        //var admin_name_modified = admin_name_no_space.replace(/'/g, '_').toLowerCase();
         var admin_id = iso.toLowerCase();
         [id_0, id_1, id_2].forEach(function(e) {
           if (e) {
