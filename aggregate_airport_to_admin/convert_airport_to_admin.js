3327959de89259d1486cb5aded914e9ace3611da
mikefab <mikefabrikant@gmail.com>
2016-12-06 19:35:42 -0500

    Remove admin_name code

--- aggregate_airport_to_admin/convert_airport_to_admin.js
+++ aggregate_airport_to_admin/convert_airport_to_admin.js
@@ -1,39 +1,31 @@
 
 
 exports.get_admin = function(lookup, airport, admin_level) {
-    // var admin = admin_level === 1 ? get_admin_1(airport) : get_admin_2(airport);
-    var admin = null;
+  // var admin = admin_level === 1 ? get_admin_1(airport) : get_admin_2(airport);
+  var admin = null;
+  if (lookup[airport]) {
+    admin = lookup[airport];
+  }
+  if (admin) {
     if (lookup[airport]) {
-      admin = lookup[airport];
-    }
-    if (admin) {
-      if (lookup[airport]) {
-        var iso = admin.ISO;
-        // These are admin codes.
-        var id_0 = admin.ID_0;
-        var id_1 = admin.ID_1;
-        var id_2 = admin.ID_2;
-	var pub_src = admin.pub_src;
-        // Create admin id with country ISO, lowercase admin name, and admin codes.
-        // Example: usa-travis-244_44-2754
-        // Temp hack 
-        //var admin_name = admin.NAME_2 || admin.NAME_1 || admin.NAME_0 || admin.NAME_ISO || admin.NOMBRE_MPI;
+      var iso = admin.ISO;
+      // These are admin codes.
+      var id_0 = admin.ID_0;
+      var id_1 = admin.ID_1;
+      var id_2 = admin.ID_2;
+      var pub_src = admin.pub_src;
 
-        //var admin_name_no_space = admin_name.replace(/(\s+|-|\.)/g, '_').toLowerCase();
-        //var admin_name_modified = admin_name_no_space.replace(/'/g, '_').toLowerCase();
-        var admin_id = iso.toLowerCase();
-        [id_0, id_1, id_2].forEach(function(e) {
-          if (e) {
-            admin_id = admin_id + '_' + e;
-          }
-        });
-	admin_id = admin_id + '_' + pub_src.replace(/-/g, '_');
-        //admin_id = admin_id + '_' + admin_name_modified.replace(/_+/g, '_');;
-        //return [iso, admin, admin_id];
-        return {iso: iso, admin_id: admin_id};
-      }
+      var admin_id = iso.toLowerCase();
+      [id_0, id_1, id_2].forEach(function(e) {
+        if (e) {
+          admin_id = admin_id + '_' + e;
+        }
+      });
+      admin_id = admin_id + '_' + pub_src.replace(/-/g, '_');
+      return {iso: iso, admin_id: admin_id};
     }
-    return null;
+  }
+  return null;
 };
 
 function get_admin_1(airport) {
