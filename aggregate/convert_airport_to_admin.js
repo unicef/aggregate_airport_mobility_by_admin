034a00c5ba5a062218b6e3f14875b796ce00ea14
mikefab <mikefabrikant@gmail.com>
2016-12-07 11:03:20 -0500

    Rename and rearrange files

---
+++ aggregate/convert_airport_to_admin.js
@@ -0,0 +1,48 @@
+
+exports.get_admin = function(lookup, airport, admin_level) {
+  // var admin = admin_level === 1 ? get_admin_1(airport) : get_admin_2(airport);
+  var admin = null;
+  if (lookup[airport]) {
+    admin = lookup[airport];
+  }
+  if (admin) {
+    if (lookup[airport]) {
+      var iso = admin.ISO;
+      // These are admin codes.
+      var id_0 = admin.ID_0;
+      var id_1 = admin.ID_1;
+      var id_2 = admin.ID_2;
+      var pub_src = admin.pub_src;
+
+      var admin_id = iso.toLowerCase();
+      [id_0, id_1, id_2].forEach(function(e) {
+        if (e) {
+          admin_id = admin_id + '_' + e;
+        }
+      });
+      admin_id = admin_id + '_' + pub_src.replace(/-/g, '_');
+      return {iso: iso, admin_id: admin_id};
+    }
+  }
+  return null;
+};
+
+function get_admin_1(airport) {
+  if (lookup[airport] && (lookup[airport].admin_1 || lookup[airport].admin_2)) {
+    if (lookup[airport].admin_1) {
+      return lookup[airport].admin_1.NAME_1;
+    } else {
+      return lookup[airport].admin_2.NAME_2;
+    }
+  }
+}
+
+function get_admin_2(airport) {
+  if (lookup[airport] && (lookup[airport].admin_1 || lookup[airport].admin_2)) {
+    if (lookup[airport].admin_2) {
+      return lookup[airport].admin_2.NAME_2;
+    } else {
+      return lookup[airport].admin_1.NAME_1;
+    }
+  }
+}
---
+++ aggregate/create_admin_to_admin_version.js
@@ -0,0 +1,127 @@
+
+// Assigns an admin to a airport.
+var admin = require('./convert_airport_to_admin');
+var fs = require('fs');
+var LineByLineReader = require('line-by-line');
+var csv_helper = require('../lib/csv_helper');
+var u = require('underscore');
+var config = require('../config');
+var separator = '\t';
+var data;
+function create_admin_to_admin_version(kind, file, db_fields, lookup) {
+  var indexes;
+  var counter = 0;
+  var records = [];
+  var lr = new LineByLineReader(config.localZippedDir + file);
+  return new Promise(function(resolve, reject) {
+    lr.on('error', function(err) {
+      console.log(err);
+         // 'err' contains error object
+    });
+    console.log('About to read lines!');
+    lr.on('line', function(line) {
+      if (counter === 0) {
+        if (line.match(/\^/)) {
+          separator = '^';
+        }
+        data = line.split(separator);
+        // It's the first line...
+        // Get indexes of needed columns
+        // For instance:
+        // { origin: 0, destination: 5, pax: 11, date: 15 }
+        indexes = csv_helper.find_indexes_for_columns(data);
+      } else if (counter !== 0 && data.length > 1) {
+        data = line.split(separator);
+        // Origin airport
+        var origin = data[indexes.origin];
+        // Destination airport
+        var destination = data[indexes.destination];
+        // Date of journey
+        // var date = data[indexes.date].split(/\s+/)[0];
+        // Number of passengers
+        var pax = parseInt(data[indexes.pax], 10);
+
+        // Get admin names for origin (an airport);
+        // origin_a2 is origin admin2
+        // var origin_a1 = admin.get_admin(origin, 1);
+        var origin_a2 = admin.get_admin(lookup, origin, 2);
+        if (!origin_a2) {
+          console.log(origin);
+        }
+        // var destination_a1 = admin.get_admin(destination, 1);
+        var destination_a2 = admin.get_admin(lookup, destination, 2);
+        var row;
+        if (origin_a2 && destination_a2 && pax) {
+          row = u.flatten([
+            origin_a2,
+            destination_a2,
+            pax
+          ]);
+
+          var json = db_fields.reduce(function(h, e, i) {
+            h[e] = row[i];
+            return h;
+          }, {});
+
+          var json = {
+            origin_id: origin_a2.admin_id,
+            origin_iso: origin_a2.iso,
+            dest_iso: destination_a2.iso,
+            dest_id: destination_a2.admin_id,
+            pax: pax
+          }
+          // records.push(new Mobility(json));
+          //if (json.origin_admin && json.dest_admin) {
+          if (json.origin_id && json.dest_id) {
+            //records.push(json);
+            records.push([json.origin_iso, json.origin_id, json.dest_iso, json.dest_id, pax].join(','));
+          }
+        }
+      }
+
+      if (counter % 50000 === 0 & counter > 0) {
+        lr.pause();
+        append_records_to_file(file, records)
+        .then(function() {
+          lr.resume();
+	});
+      }
+      counter += 1;
+    });
+
+    lr.on('end', function() {
+      if (records.length > 0) {
+        append_records_to_file(file, records)
+        .then(function() { resolve() });
+      } else {
+        console.log('Done importing', file);
+        resolve();
+      }
+    });
+  });
+}
+
+function append_records_to_file(file, records) {
+  return new Promise(function(resolve, reject) {
+    fs.appendFile(config.localTransformedDir + file, records.join('\n'), function(err) {
+      if (err) {
+        console.log(err);
+      }
+      resolve();
+    });
+  });
+}
+exports.create_admin_to_admin_version = function(kind, file, db_fields, lookup) {
+  return new Promise(function(resolve, reject) {
+    var fs = require('fs');
+    fs.writeFile(config.localTransformedDir  + file, db_fields + '\n', function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      create_admin_to_admin_version(kind, file, db_fields, lookup)
+      .then(function() {
+        resolve();
+      });
+    });
+  });
+};
