67cda04a9eeef49a5628efea94f8ffe1c7eda71f
mikefab <mikefabrikant@gmail.com>
2016-12-25 18:49:12 +0000

    Aggegate differently depending on collection type

--- aggregate/spark_aggregate.js
+++ aggregate/spark_aggregate.js
@@ -2,10 +2,11 @@ var sys = require('sys')
 var exec = require('child_process').exec;
 var child;
 // executes `pwd`
-exports.aggregate = function(file) {
+exports.aggregate = function(file, collection) {
   console.log('SPARK CLI!', file);
   return new Promise(function(resolve, reject) {
-      child = exec('../../spark-2.0.2-bin-hadoop2.7/bin/spark-shell -i ./spark/aggregate.scala --conf spark.driver.extraJavaOptions="-D' + file + '"', function (error, stdout, stderr) {
+      child = exec(
+        '../../spark-2.0.2-bin-hadoop2.7/bin/spark-shell -i ./spark/aggregate.scala --conf spark.driver.extraJavaOptions="-D' + file + ',' + collection + '"', function (error, stdout, stderr) {
       sys.print('stdout: ' + stdout);
       sys.print('stderr: ' + stderr);
       if (error !== null) {
