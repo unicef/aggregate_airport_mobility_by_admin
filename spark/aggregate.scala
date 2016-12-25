5e87976074bbe579679719821cb23f89362021af
mikefab <mikefabrikant@gmail.com>
2016-12-25 18:49:52 +0000

    Aggegate differently depending on collection type

--- spark/aggregate.scala
+++ spark/aggregate.scala
@@ -6,8 +6,14 @@ val paramsString = sconf.get("spark.driver.extraJavaOptions")
 val paramsSlice = paramsString.slice(2,paramsString.length)
 val paramsArray = paramsSlice.split(",")
 val arg1 = paramsArray(0)
+val collection = paramsArray(1);
+val statement = if (collection == "traffic") {
+  "select year, week, sum(pax) as cnt, first_value(origin_iso), origin_id, first_value(dest_iso), dest_id from mobils group by origin_id, dest_id, year, week order by cnt" } else {
+   "select date, sum(pax) as cnt, first_value(origin_iso), origin_id, first_value(dest_iso), dest_id from mobils group by origin_id, dest_id, date order by cnt"
+}
 val mobil = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", ",").load("./temp/transformed/" + arg1)
 mobil.registerTempTable("mobils")
-val distOrigins = sqlContext.sql("select year, week, sum(pax) as cnt, first_value(origin_iso), origin_id, first_value(dest_iso), dest_id from mobils group by origin_id, dest_id, year, week order by cnt")
+println(statement)
+val distOrigins = sqlContext.sql(statement)
 distOrigins.write.format("com.databricks.spark.csv").save("./temp/processed/" + arg1)
 System.exit(0)
